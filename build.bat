936fe10bc246a80298130ec47837954dba183785
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-04-20 13:12:03 +0200

    Update build-script

--- build.bat
+++ build.bat
@@ -1,5 +1,29 @@
 @echo off
-cd src
-powershell Compress-Archive -Path * -DestinationPath "..\builds\whosh.zip"
+
+rem Define paths
+set "src_path=src"
+set "build_path=builds"
+set "zip_file=%build_path%\whosh.zip"
+set "mpack_file=%build_path%\whosh.mpack"
+
+rem Check if mpack file exists in the build folder and delete if it does
+if exist "%mpack_file%" (
+    del "%mpack_file%"
+)
+
+rem Check if zip file exists in the build folder and delete if it does
+if exist "%zip_file%" (
+    del "%zip_file%"
+)
+
+rem Change directory to src
+cd "%src_path%"
+
+rem Create a new zip file named whosh.zip
+powershell Compress-Archive -Path * -DestinationPath "..\%zip_file%"
+
+rem Change directory back to the parent directory
 cd ..
-ren builds\whosh.zip whosh.mpack
+
+rem Rename whosh.zip to whosh.mpack
+ren "%zip_file%" whosh.mpack
